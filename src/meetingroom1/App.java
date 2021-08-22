d11feaa09208f9e81d7eacf552000c08625db01e
krishna <krishnaprasad.vk@gmail.com>
2021-08-23 00:52:28 +0530

    Removing incorrect files

--- src/meetingroom1/App.java
+++
@@ -1,9 +0,0 @@
-package meetingroom1;
-
-public class App {
-    public static void main(String[] args) {
-        int[][] intervals = {{20, 21}, {5, 10}, {10, 20}};
-        Solution solution = new Solution();
-        System.out.println("Will I be able to attend all meetings? : " + solution.canAttendMeetings(intervals));
-    }
-}
--- src/meetingroom2/App.java
+++
@@ -1,9 +0,0 @@
-package meetingroom2;
-
-public class App {
-    public static void main(String[] args) {
-        int[][] intervals = {{8, 12}, {5, 10}, {10, 20},{15,20},{1,6}};
-        Solution solution = new Solution();
-        System.out.println("Min meeting rooms required : " + solution.minMeetingRooms(intervals));
-    }
-}
