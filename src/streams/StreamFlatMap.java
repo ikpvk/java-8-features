208b00fd428c103ac8b6b838737d794242300773
Krishna Prasad <krishnaprasad.vk@gmail.com>
2024-04-28 18:40:16 +0530

    Adding Flat Map example
    Adding Flat Map example

---
+++ src/streams/StreamFlatMap.java
@@ -0,0 +1,42 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamFlatMap {
+    public static void main(String[] args) {
+        List<String> listStrs = Arrays.asList("Geeks", "GFG",
+                "GeeksforGeeks", "gfg");
+
+        System.out.println("Basics");
+        listStrs.stream().flatMap(str ->
+                        Stream.of(str.charAt(0))).
+                forEach(System.out::println);
+
+        System.out.println("Advanced");
+        // Creating a list of Prime Numbers
+        List<Integer> PrimeNumbers = Arrays.asList(5, 7, 11, 13);
+
+        // Creating a list of Odd Numbers
+        List<Integer> OddNumbers = Arrays.asList(1, 3, 5);
+
+        // Creating a list of Even Numbers
+        List<Integer> EvenNumbers = Arrays.asList(2, 4, 6, 8);
+
+        List<List<Integer>> listOfListofInts =
+                Arrays.asList(PrimeNumbers, OddNumbers, EvenNumbers);
+
+        System.out.println("The Structure before flattening is : " +
+                listOfListofInts);
+
+        // Using flatMap for transformating and flattening.
+        List<Integer> listofInts = listOfListofInts.stream()
+                .flatMap(list -> list.stream())
+                .collect(Collectors.toList());
+
+        System.out.println("The Structure after flattening is : " +
+                listofInts);
+    }
+}
